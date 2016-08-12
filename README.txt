6522653d56ec92205999dbb3d1cdb9a8dbc8a393
Andy Weidenbaum <archbaum@gmail.com>
2016-08-11 17:44:43 -0700

    Initial import

---
+++ README.txt
@@ -0,0 +1,10 @@
+/* vim: set ft=journal fdm=marker fdl=0 nowrap: */
+
+Homebrew formulae for Mac OS X
+
+
+Licensing
+---------
+
+This is free and unencumbered public domain software. For more
+information, see http://unlicense.org/ or the accompanying UNLICENSE file.
---
+++ tmux/tmux.rb
@@ -0,0 +1,61 @@
+class Tmux < Formula
+  desc "Terminal multiplexer"
+  homepage "https://tmux.github.io/"
+
+  head do
+    url "https://github.com/tmux/tmux.git"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
+  depends_on "pkg-config" => :build
+  depends_on "libevent"
+
+  resource "completion" do
+    url "https://raw.githubusercontent.com/imomaliev/tmux-bash-completion/homebrew_1.0.0/completions/tmux"
+    sha256 "05e79fc1ecb27637dc9d6a52c315b8f207cf010cdcee9928805525076c9020ae"
+  end
+
+  def install
+    system "sh", "autogen.sh" if build.head?
+
+    ENV.append "LDFLAGS", "-lresolv"
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}",
+                          "--sysconfdir=#{etc}"
+
+    system "make", "install"
+
+    pkgshare.install "example_tmux.conf"
+    bash_completion.install resource("completion")
+  end
+
+  def caveats; <<-EOS.undent
+    Example configuration has been installed to:
+      #{opt_pkgshare}
+    EOS
+  end
+
+  test do
+    system "#{bin}/tmux", "-V"
+  end
+end
+
+__END__
+diff --git a/window-copy.c b/window-copy.c
+index dfd0dc0..eea693b 100644
+--- a/window-copy.c
++++ b/window-copy.c
+@@ -689,8 +689,8 @@ window_copy_key(struct window_pane *wp, struct client *c, struct session *sess,
+ 	case MODEKEYCOPY_COPYPIPE:
+ 		if (sess != NULL) {
+ 			window_copy_copy_pipe(wp, sess, NULL, arg);
+-			window_pane_reset_mode(wp);
+-			return;
++			window_copy_clear_selection(wp);
++			window_copy_redraw_screen(wp);
+ 		}
+ 		break;
+ 	case MODEKEYCOPY_COPYSELECTION:
