d695c74ac80b88dff390c40d48044cbe88994001
Andy Weidenbaum <archbaum@gmail.com>
2016-08-12 09:15:50 -0700

    add peru formula

---
+++ peru/peru.rb
@@ -0,0 +1,42 @@
+require 'formula'
+
+class Peru < Formula
+  homepage 'https://github.com/buildinspace/peru'
+  url 'https://github.com/buildinspace/peru/archive/1.0.0.tar.gz'
+  sha256 '386936ce853ed4d9d9b801d1c7d05295e54ceafebbb86fb89e78e27f504f6045'
+
+  depends_on 'git'
+  depends_on 'libyaml'
+  depends_on 'python3'
+
+  resource 'docopt' do
+    url 'https://pypi.python.org/packages/a2/55/8f8cab2afd404cf578136ef2cc5dfb50baa1761b68c9da1fb1e4eed343c9/docopt-0.6.2.tar.gz'
+    sha256 '49b3a825280bd66b3aa83585ef59c4a8c82f2c8a522dbe754a8bc8d08c85c491'
+  end
+
+  resource 'yaml' do
+    url 'https://pypi.python.org/packages/75/5e/b84feba55e20f8da46ead76f14a3943c8cb722d40360702b2365b91dec00/PyYAML-3.11.tar.gz'
+    sha256 'c36c938a872e5ff494938b33b14aaa156cb439ec67548fcab3535bb78b0846e8'
+  end
+
+  def install
+    ENV["PYTHONPATH"] = lib+"python3.5/site-packages"
+    ENV.prepend_create_path 'PYTHONPATH', libexec+'lib/python3.5/site-packages'
+    ENV.prepend_create_path 'PYTHONPATH', prefix+'lib/python3.5/site-packages'
+    install_args = [ "setup.py", "install", "--prefix=#{libexec}" ]
+
+    res = %w[docopt yaml]
+    res.each do |r|
+      resource(r).stage { system "python3", *install_args }
+    end
+
+    system "python3", "setup.py", "build"
+    system "python3", "setup.py", "install", "--prefix=#{prefix}", "--optimize=1"
+
+    bin.env_script_all_files(libexec+'bin', :PYTHONPATH => ENV['PYTHONPATH'])
+  end
+
+  test do
+    system "#{bin}/peru", "--version"
+  end
+end
