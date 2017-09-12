09e0cf4cbad7d55dcb4d49aa1643823b2d44f243
Andy Weidenbaum <archbaum@gmail.com>
2017-09-12 08:57:45 -0700

    update libxxhash to 0.6.3, -DBUILD_SHARED_LIBS=ON

--- libxxhash/libxxhash.rb
+++ libxxhash/libxxhash.rb
@@ -1,15 +1,18 @@
 class Libxxhash < Formula
   desc "Extremely fast non-cryptographic hash algorithm (library)"
   homepage "https://cyan4973.github.io/xxHash/"
-  url "https://github.com/Cyan4973/xxHash/archive/v0.6.2.tar.gz"
-  sha256 "e4da793acbe411e7572124f958fa53b280e5f1821a8bf78d79ace972950b8f82"
+  url "https://github.com/Cyan4973/xxHash/archive/v0.6.3.tar.gz"
+  sha256 "d8c739ec666ac2af983a61dc932aaa2a8873df974d333a9922d472a121f2106e"
 
   depends_on "cmake" => :build
 
   def install
     system "mkdir", "-p", "build"
     Dir.chdir("build")
-    system "cmake", "-DCMAKE_INSTALL_PREFIX=#{prefix}", "../cmake_unofficial/"
+    system "cmake",
+           "-DCMAKE_INSTALL_PREFIX=#{prefix}",
+           "-DBUILD_SHARED_LIBS=ON",
+           "../cmake_unofficial/"
     system "make", "all"
     system "make", "install"
   end
