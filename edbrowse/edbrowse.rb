d417150de732ff68d23a35dbd40bfc7bc8c15044
Andy Weidenbaum <archbaum@gmail.com>
2018-10-08 08:30:02 -0500

    edbrowse: fix head

--- edbrowse/edbrowse.rb
+++ edbrowse/edbrowse.rb
@@ -3,10 +3,7 @@ class Edbrowse < Formula
   homepage "http://www.edbrowse.org"
   url "https://github.com/CMB/edbrowse/archive/v3.7.4.tar.gz"
   sha256 "b79e6417c1170c96ef68968c33d585725f0f2d27859e3fe807c980a9c33ea719"
-
-  head do
-    url "https://github.com/CMB/edbrowse"
-  end
+  head "https://github.com/CMB/edbrowse.git"
 
   depends_on "curl"
   depends_on "duktape"
