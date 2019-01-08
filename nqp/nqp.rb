26fef2a8cc4f71f931e28602ed0075ad7c2a34eb
Andy Weidenbaum <archbaum@gmail.com>
2019-01-08 13:18:32 +1300

    perl6: merge upstream changes

--- nqp/nqp.rb
+++ nqp/nqp.rb
@@ -3,27 +3,20 @@ class Nqp < Formula
   homepage "https://github.com/perl6/nqp"
   url "https://rakudo.perl6.org/downloads/nqp/nqp-2018.12.tar.gz"
   sha256 "219db519ad5c1848e4528a56a506dd74b0839ca1d910788411f3bfedf5045d36"
-  head "https://github.com/perl6/nqp.git"
 
-  depends_on "make" => :build
-  depends_on "perl" => :build
   depends_on "moarvm"
 
   def install
-    moarvm = Formula["moarvm"]
-    configure_args = [
-      "--backends=moar",
-      "--prefix=#{prefix}",
-      "--with-moar=#{moarvm.bin}/moar",
-    ]
-    system "perl", "Configure.pl", *configure_args
+    system "perl", "Configure.pl",
+                   "--backends=moar",
+                   "--prefix=#{prefix}",
+                   "--with-moar=#{Formula["moarvm"].bin}/moar"
     system "make"
     system "make", "install"
   end
 
   test do
-    out = `#{bin}/nqp -e 'for (0,1,2,3,4,5,6,7,8,9) { print($_) }'`
+    out = shell_output("#{bin}/nqp -e 'for (0,1,2,3,4,5,6,7,8,9) { print($_) }'")
     assert_equal "0123456789", out
-    assert_equal 0, $CHILD_STATUS.exitstatus
   end
 end
--- rakudo/rakudo.rb
+++ rakudo/rakudo.rb
@@ -1,33 +1,23 @@
 class Rakudo < Formula
-  desc "Production-ready, stable implementation of the Perl 6 language"
+  desc "Perl 6 compiler targeting MoarVM"
   homepage "https://rakudo.org"
   url "https://rakudo.perl6.org/downloads/rakudo/rakudo-2018.12.tar.gz"
   sha256 "67bb02b9afd4f2a2a8542e25ce2691bc2d77864668f0fc82e1d39dea31a584fa"
-  head "https://github.com/rakudo/rakudo.git"
 
-  depends_on "make" => :build
-  depends_on "perl" => :build
   depends_on "nqp"
 
   def install
-    nqp = Formula["nqp"]
-    configure_args = [
-      "--backends=moar",
-      "--prefix=#{prefix}",
-      "--with-nqp=#{nqp.bin}/nqp",
-    ]
-    system "perl", "Configure.pl", *configure_args
+    system "perl", "Configure.pl",
+                   "--backends=moar",
+                   "--prefix=#{prefix}",
+                   "--with-nqp=#{Formula["nqp"].bin}/nqp"
     system "make"
-    ENV["RAKUDO_LOG_PRECOMP"] = "1"
-    ENV["RAKUDO_RERESOLVE_DEPENDENCIES"] = "0"
     system "make", "install"
-    mv "tools/install-dist.p6", "tools/perl6-install-dist"
-    bin.install "tools/perl6-install-dist"
+    bin.install "tools/install-dist.p6" => "perl6-install-dist"
   end
 
   test do
-    out = `#{bin}/perl6 -e 'loop (my $i = 0; $i < 10; $i++) { print $i }'`
+    out = shell_output("#{bin}/perl6 -e 'loop (my $i = 0; $i < 10; $i++) { print $i }'")
     assert_equal "0123456789", out
-    assert_equal 0, $CHILD_STATUS.exitstatus
   end
 end
