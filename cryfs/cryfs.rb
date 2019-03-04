82a17fb92e7971a3da2d1addd17c18239c20df78
Andy Weidenbaum <archbaum@gmail.com>
2019-03-05 09:14:36 +1300

    cryfs: update to 0.10.0

--- cryfs/cryfs.rb
+++ cryfs/cryfs.rb
@@ -1,35 +1,29 @@
 class Cryfs < Formula
   desc "Cryptographic filesystem for the cloud"
   homepage "https://www.cryfs.org"
-  url "https://github.com/cryfs/cryfs/releases/download/0.9.9/cryfs-0.9.9.tar.xz"
-  sha256 "aa8d90bb4c821cf8347f0f4cbc5f68a1e0f4eb461ffd8f1ee093c4d37eac2908"
+  url "https://github.com/cryfs/cryfs/releases/download/0.10.0/cryfs-0.10.0.tar.xz"
+  sha256 "2413b76ad73026eb166c72c2081d303eafe2357d6a981618765fc4720281e52b"
 
   head do
     url "https://github.com/cryfs/cryfs.git", branch: "develop", :shallow => false
-    depends_on "libomp"
   end
 
+  depends_on "cmake" => :build
   depends_on "boost"
-  depends_on "cryptopp"
+  depends_on "libomp"
   depends_on "openssl"
   depends_on :osxfuse
-  depends_on "cmake" => :build
-  depends_on "python" => :build
-
-  needs :cxx11
 
   def install
     libomp = Formula['libomp']
     configure_args = [
       "-DBUILD_TESTING=off",
       "-DCMAKE_BUILD_TYPE=Release",
-      "-DCRYFS_UPDATE_CHECKS=off"
-    ]
-    configure_args.concat([
+      "-DCRYFS_UPDATE_CHECKS=off",
       "-DOpenMP_CXX_FLAGS='-Xpreprocessor -fopenmp -I#{libomp.include}'",
       "-DOpenMP_CXX_LIB_NAMES=omp",
       "-DOpenMP_omp_LIBRARY=#{libomp.lib}/libomp.dylib"
-    ]) if build.head?
+    ]
     ncpu = `sysctl -n hw.ncpu`.to_i
     mkdir("build") do
       system "cmake", *configure_args, *std_cmake_args, ".."
@@ -40,6 +34,17 @@ class Cryfs < Formula
 
   test do
     ENV["CRYFS_FRONTEND"] = "noninteractive"
-    assert_match "CryFS", shell_output("#{bin}/cryfs", 10)
+
+    # Test showing help page
+    assert_match "CryFS", shell_output("#{bin}/cryfs 2>&1", 10)
+
+    # Test mounting a filesystem. This command will ultimately fail because homebrew tests
+    # don't have the required permissions to mount fuse filesystems, but before that
+    # it should display "Mounting filesystem". If that doesn't happen, there's something
+    # wrong. For example there was an ABI incompatibility issue between the crypto++ version
+    # the cryfs bottle was compiled with and the crypto++ library installed by homebrew to.
+    mkdir "basedir"
+    mkdir "mountdir"
+    assert_match "Operation not permitted", pipe_output("#{bin}/cryfs -f basedir mountdir 2>&1", "password")
   end
 end
