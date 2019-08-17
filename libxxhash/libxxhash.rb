d9235f5dbb6a9b9068836cfba53f06ecc4061a2d
Andy Weidenbaum <archbaum@gmail.com>
2019-08-17 08:06:37 +0100

    libxxhash 0.7.1

--- libxxhash/libxxhash.rb
+++ libxxhash/libxxhash.rb
@@ -1,8 +1,8 @@
 class Libxxhash < Formula
   desc "Extremely fast non-cryptographic hash algorithm (library)"
   homepage "https://cyan4973.github.io/xxHash/"
-  url "https://github.com/Cyan4973/xxHash/archive/v0.7.0.tar.gz"
-  sha256 "b34792646d5e19964bb7bba24f06cb13aecaac623ab91a54da08aa19d3686d7e"
+  url "https://github.com/Cyan4973/xxHash/archive/v0.7.1.tar.gz"
+  sha256 "afa29766cfc0448ff4a1fd9f2c47e02c48d50be5b79749925d15d545008c3f81"
   head "https://github.com/Cyan4973/xxHash.git"
 
   def install
