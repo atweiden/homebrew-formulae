61c1cccb8b40422e8e5c9c63e8f6611979c34282
Andy Weidenbaum <archbaum@gmail.com>
2019-11-27 10:32:34 +0900

    tmux 3.0

--- tmux/tmux.rb
+++ tmux/tmux.rb
@@ -1,17 +1,9 @@
 class Tmux < Formula
   desc "Terminal multiplexer"
   homepage "https://tmux.github.io/"
-  url "https://github.com/tmux/tmux/releases/download/2.9a/tmux-2.9a.tar.gz"
-  sha256 "839d167a4517a6bffa6b6074e89a9a8630547b2dea2086f1fad15af12ab23b25"
+  url "https://github.com/tmux/tmux/releases/download/3.0/tmux-3.0.tar.gz"
+  sha256 "9edcd78df80962ee2e6471a8f647602be5ded62bb41c574172bb3dc3d0b9b4b4"
   revision 1
-
-  bottle do
-    cellar :any
-    sha256 "9479fe351b5e6165ac6c50b6cdd393afa84fc35e908696a6b0410f2f8cf8dd01" => :catalina
-    sha256 "a6c847ffc57c9e6d730b1dcfb3ca193588cfdb679a60f221a41690ac8ec202a7" => :mojave
-    sha256 "c01b0af8cf9a266e4ced0aaa6c05bd9c83b69b22ea9c790f0ab38eec4d86fcbf" => :high_sierra
-    sha256 "caeb4047951019c086ddbb8d0ace71b3f62e7266decd2b06c28f0213897292b4" => :sierra
-  end
 
   head do
     url "https://github.com/tmux/tmux.git"
@@ -26,7 +18,7 @@ class Tmux < Formula
   depends_on "ncurses"
 
   resource "completion" do
-    url "https://raw.githubusercontent.com/imomaliev/tmux-bash-completion/homebrew_1.0.0/completions/tmux"
+    url "https://github.com/imomaliev/tmux-bash-completion/raw/homebrew_1.0.0/completions/tmux"
     sha256 "05e79fc1ecb27637dc9d6a52c315b8f207cf010cdcee9928805525076c9020ae"
   end
 
