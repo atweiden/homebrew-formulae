012918e8cf2f2a1502342dfc8ca4b47b0900eb3d
Andy Weidenbaum <archbaum@gmail.com>
2020-05-19 02:53:05 +1000

    add freepass formula

---
+++ freepass/freepass.rb
@@ -0,0 +1,29 @@
+class Freepass < Formula
+  desc "Free password manager for power users - CLI"
+  homepage "https://github.com/myfreeweb/freepass"
+
+  stable do
+    # submodules are cloned automatically
+    url "https://github.com/myfreeweb/freepass.git",
+      :revision => "7861153baae6f557faacecbda5412b307ca220b8"
+    version "0.0.0"
+  end
+
+  head do
+    url "https://github.com/myfreeweb/freepass.git"
+  end
+
+  depends_on "rust" => :build
+  depends_on "libsodium"
+  depends_on "llvm"
+
+  def install
+    mkdir("cli") do
+      system "cargo", "install", "--locked", "--root", prefix, "--path", "."
+    end
+  end
+
+  test do
+    true
+  end
+end
