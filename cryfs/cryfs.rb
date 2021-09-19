88085720d867554aace696cecda8b6ab004a8c67
Andy Weidenbaum <atweiden@ioiojo.com>
2021-09-19 14:40:09 +1000

    cryfs 0.11.0
    credit
    - https://github.com/cryfs/homebrew-tap

--- cryfs/cryfs.rb
+++ cryfs/cryfs.rb
@@ -1,35 +1,55 @@
+class MacfuseRequirement < Requirement
+  fatal true
+
+  satisfy(build_env: false) { self.class.binary_macfuse_installed? }
+
+  def self.binary_macfuse_installed?
+    File.exist?("/usr/local/include/fuse/fuse.h") &&
+      !File.symlink?("/usr/local/include/fuse")
+  end
+
+  def message
+    "macFUSE is required to build cryfs. Please run `brew install --cask macfuse` first."
+  end
+end
+
 class Cryfs < Formula
-  desc "Cryptographic filesystem for the cloud"
+  desc "Encrypts your files so you can safely store them in Dropbox, iCloud, etc."
   homepage "https://www.cryfs.org"
-  url "https://github.com/cryfs/cryfs/releases/download/0.10.2/cryfs-0.10.2.tar.xz"
-  sha256 "5531351b67ea23f849b71a1bc44474015c5718d1acce039cf101d321b27f03d5"
+  license "LGPL-3.0-or-later"
+
+  stable do
+    url "https://github.com/cryfs/cryfs/releases/download/0.11.0/cryfs-0.11.0.tar.xz"
+    sha256 "5583f84f3fcbd4bdbdcc9bfe4bb10971b2fca80a67b539b340556b5de482b737"
+  end
 
   head do
-    url "https://github.com/cryfs/cryfs.git", branch: "develop", :shallow => false
+    url "https://github.com/cryfs/cryfs.git", branch: "develop", shallow: false
   end
 
   depends_on "cmake" => :build
-  depends_on "boost"
+  depends_on "conan" => :build
+  depends_on "ninja" => :build
+  depends_on "pkg-config" => :build
+  depends_on "curl"
   depends_on "libomp"
-  depends_on "openssl"
-  depends_on :osxfuse
+  depends_on "openssl@1.1"
+  depends_on MacfuseRequirement
 
   def install
-    libomp = Formula['libomp']
     configure_args = [
+      "-GNinja",
       "-DBUILD_TESTING=off",
       "-DCMAKE_BUILD_TYPE=Release",
       "-DCRYFS_UPDATE_CHECKS=off",
-      "-DOpenMP_CXX_FLAGS='-Xpreprocessor -fopenmp -I#{libomp.include}'",
-      "-DOpenMP_CXX_LIB_NAMES=omp",
-      "-DOpenMP_omp_LIBRARY=#{libomp.lib}/libomp.dylib"
     ]
-    ncpu = `sysctl -n hw.ncpu`.to_i
-    mkdir("build") do
-      system "cmake", *configure_args, *std_cmake_args, ".."
-      system "make", "-j#{ncpu}"
-      system "make", "install", "prefix=#{prefix}"
+
+    # macFUSE puts pkg-config into /usr/local/lib/pkgconfig, which is not included in
+    # homebrew's default PKG_CONFIG_PATH. We need to tell pkg-config about this path for our build
+    with_env "PKG_CONFIG_PATH" => ENV["PKG_CONFIG_PATH"] + ":/usr/local/lib/pkgconfig" do
+      system "cmake", ".", *configure_args, *std_cmake_args
     end
+    system "ninja", "install"
   end
 
   test do
@@ -38,11 +58,13 @@ class Cryfs < Formula
     # Test showing help page
     assert_match "CryFS", shell_output("#{bin}/cryfs 2>&1", 10)
 
-    # Test mounting a filesystem. This command will ultimately fail because homebrew tests
-    # don't have the required permissions to mount fuse filesystems, but before that
-    # it should display "Mounting filesystem". If that doesn't happen, there's something
-    # wrong. For example there was an ABI incompatibility issue between the crypto++ version
-    # the cryfs bottle was compiled with and the crypto++ library installed by homebrew to.
+    # Test mounting a filesystem. This command will ultimately fail with "Operation not permitted"
+    # because homebrew tests don't have the required permissions to mount fuse filesystems.
+    # So this doesn't really test CryFS functionality, but at least it tests that CryFS tries to
+    # access something that homebrew tests don't have access to...
+    # This is still helpful. For example there was an ABI incompatibility issue between the crypto++
+    # version the cryfs bottle was compiled with and the crypto++ library installed by homebrew to,
+    # this test catches such things.
     mkdir "basedir"
     mkdir "mountdir"
     assert_match "Operation not permitted", pipe_output("#{bin}/cryfs -f basedir mountdir 2>&1", "password")
