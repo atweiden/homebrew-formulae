24643ea58ced6e4cb68a1c755d81d5a7dc6c08fd
atweiden <atweiden@ioiojo.com>
2022-05-03 09:35:16 +0800

    duktape 2.7.0

--- duktape/duktape.rb
+++ duktape/duktape.rb
@@ -1,30 +1,33 @@
-# Credit: https://github.com/Homebrew/homebrew-core/pull/28480
 class Duktape < Formula
   desc "Embeddable Javascript engine with compact footprint"
   homepage "https://duktape.org"
-  url "https://duktape.org/duktape-2.4.0.tar.xz"
-  sha256 "86a89307d1633b5cedb2c6e56dc86e92679fc34b05be551722d8cc69ab0771fc"
+  url "https://duktape.org/duktape-2.7.0.tar.xz"
+  sha256 "90f8d2fa8b5567c6899830ddef2c03f3c27960b11aca222fa17aa7ac613c2890"
+  license "MIT"
 
   def install
-    make_args = ["-f", "Makefile.sharedlibrary"]
-    make_install_args = ["INSTALL_PREFIX=#{prefix}", *make_args]
-    system "make", *make_args
-    system "make", *make_install_args, "install"
+    ENV["INSTALL_PREFIX"] = prefix
+    system "make", "-f", "Makefile.sharedlibrary", "install"
+    system "make", "-f", "Makefile.cmdline"
+    bin.install "duk"
   end
 
   test do
+    (testpath/"test.js").write "console.log('Hello Homebrew!');"
+    assert_equal "Hello Homebrew!", shell_output("#{bin}/duk test.js").strip
+
     (testpath/"test.cc").write <<~EOS
       #include <stdio.h>
-      #include \"duktape.h\"
+      #include "duktape.h"
       int main(int argc, char *argv[]) {
         duk_context *ctx = duk_create_heap_default();
-        duk_eval_string(ctx, \"1+2\");
-        printf(\"1+2=%d\\n\", (int) duk_get_int(ctx, -1));
+        duk_eval_string(ctx, "1 + 2");
+        printf("1 + 2 = %d\\n", (int) duk_get_int(ctx, -1));
         duk_destroy_heap(ctx);
         return 0;
       }
     EOS
-    system ENV.cc, "-I#{include}", "-lduktape", testpath/"test.cc", "-o", testpath/"test"
-    assert_equal "1+2=3", shell_output(testpath/"test").strip, "Duktape can add number"
+    system ENV.cc, "test.cc", "-o", "test", "-I#{include}", "-L#{lib}", "-lduktape", "-lm"
+    assert_equal "1 + 2 = 3", shell_output("./test").strip, "Duktape can add number"
   end
 end
