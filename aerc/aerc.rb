ef33d170ae8c0763802b79c403791167b4a8275d
atweiden <atweiden@ioiojo.com>
2022-05-03 09:32:50 +0800

    aerc 0.9.0

--- aerc/aerc.rb
+++ aerc/aerc.rb
@@ -1,11 +1,14 @@
 class Aerc < Formula
-  desc "Pretty good email client"
+  desc "Email client that runs in your terminal"
   homepage "https://aerc-mail.org/"
-  url "https://git.sr.ht/~sircmpwn/aerc/archive/0.4.0.tar.gz"
-  sha256 "d369462cc0f76c33d804e586463e4d35d81fba9396ec08c3d3d0579e26091e17"
+  url "https://git.sr.ht/~rjarry/aerc/archive/0.9.0.tar.gz"
+  sha256 "b5901feb37a55edd1f713e76c1012ac3fc0757202ddacd7d388cc7ce60638023"
+  license "MIT"
+  head "https://git.sr.ht/~rjarry/aerc", branch: "master"
 
   depends_on "go" => :build
   depends_on "scdoc" => :build
+  # for showing HTML messages
   depends_on "dante"
   depends_on "w3m"
 
