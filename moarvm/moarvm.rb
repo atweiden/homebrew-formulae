d00e360af238593208e74c8c05cfa4d8da8481d1
Andy Weidenbaum <atweiden@ioiojo.com>
2022-08-07 09:06:49 +1000

    moarvm 2022.07

---
+++ moarvm/moarvm.rb
@@ -0,0 +1,51 @@
+class Moarvm < Formula
+  desc "Virtual machine for NQP and Rakudo Perl 6"
+  homepage "https://moarvm.org"
+  url "https://moarvm.org/releases/MoarVM-2022.07.tar.gz"
+  sha256 "337ef04d16f826f99465c653b92006028fe220be68d3dcfd0729612f4f6b5b46"
+  license "Artistic-2.0"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  depends_on "pkg-config" => :build
+  depends_on "libffi"
+  depends_on "libtommath"
+  depends_on "libuv"
+  depends_on "zstd"
+
+  uses_from_macos "perl" => :build
+
+  conflicts_with "rakudo-star", because: "rakudo-star currently ships with moarvm included"
+
+  resource "nqp" do
+    url "https://rakudo.org/dl/nqp/nqp-2022.07.tar.gz"
+    sha256 "58081c106d672a5406018fd69912c8d485fd12bf225951325c50c929a8232268"
+  end
+
+  def install
+    configure_args = %W[
+      --c11-atomics
+      --has-libffi
+      --has-libtommath
+      --has-libuv
+      --optimize
+      --pkgconfig=#{Formula["pkg-config"].opt_bin}/pkg-config
+      --prefix=#{prefix}
+    ]
+    system "perl", "Configure.pl", *configure_args
+    system "make", "realclean"
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    testpath.install resource("nqp")
+    out = Dir.chdir("src/vm/moar/stage0") do
+      shell_output("#{bin}/moar nqp.moarvm -e 'for (0,1,2,3,4,5,6,7,8,9) { print($_) }'")
+    end
+    assert_equal "0123456789", out
+  end
+end
