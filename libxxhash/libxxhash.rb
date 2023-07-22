ec0546fd258cbdcf0aeb7c8387d775374eb91499
Andy Weidenbaum <atweiden@ioiojo.com>
2023-07-22 02:28:41 +0000

    libxxhash 0.8.2

--- libxxhash/libxxhash.rb
+++ libxxhash/libxxhash.rb
@@ -1,8 +1,8 @@
 class Libxxhash < Formula
   desc "Extremely fast non-cryptographic hash algorithm (library)"
   homepage "https://cyan4973.github.io/xxHash/"
-  url "https://github.com/Cyan4973/xxHash/archive/v0.8.1.tar.gz"
-  sha256 "3bb6b7d6f30c591dd65aaaff1c8b7a5b94d81687998ca9400082c739a690436c"
+  url "https://github.com/Cyan4973/xxHash/archive/v0.8.2.tar.gz"
+  sha256 "baee0c6afd4f03165de7a4e67988d16f0f2b257b51d0e3cb91909302a26a79c4"
   head "https://github.com/Cyan4973/xxHash.git"
   license "BSD-2-Clause"
 
